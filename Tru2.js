4c55b690993f0b7da2f5880e36a3b355e7afac8e
trum21 <80861964+trum21@users.noreply.github.com>
2021-03-18 17:54:02 +0300

    Create Tru2.js

---
+++ Tru2.js
@@ -0,0 +1,36 @@
+$(`<img src="/sico/z1etg5qg3p10.jpg" class="fr" style="width:100%;margin-top: 0px;" > 
+
+<a id="des3" href="" style="border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:73px;margin:2px 2px 2px;" class="fl mini fa fa-phone btn btn-warning"> الأداره </a> <a id="des3" href="" style="border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:73px;margin:2px 2px 2px;" class="fl mini fa fa-paint-brush btn btn-danger"> التطبيق </a> <a id="des3" href="" style="border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:73px;margin:2px 2px 2px;" class="fl mini fa fa-star btn btn-success"> اشتراك </a> <a id="des3" href="" style="border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:73px;margin:2px 2px 2px;" class="fl mini fa fa-image btn btn-warning"> رمزيات </a> <a id="des3" href="" style="border: 1px solid white; border-radius: 0px 15px 0px 0px!important;width:73px;margin:2px 2px 2px;" class="fl mini fa fa-heartbeat btn btn-info"> شات </a> `).insertBefore('.nav-tabs'); 
+
+$('.break.light').css({"background-image":"url(/sico/z1e9emlrq310.jpeg)"}); 
+$(".fa-gear").text("الضبط").css("width","");  
+
+$(".ubnr").remove(); 
+
+$(".nonot").remove(); 
+
+$('.u-msg.dots').removeClass('dots'); 
+
+$(".label-primary").css("border-radius","24px 24px 24px 24px") 
+
+
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="jfa-w" name="FirstName" value=" قرآن كريم "> <audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="https://h.top4top.io/m_1595scph70.mp3"><a href="https://h.top4top.io/m_1595scph70.mp3">https://h.top4top.io/m_1595scph70.mp3</a></audio> <center></center></center> </div>`); $('#d0').append(`<label title="الاذاعة" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();" data-toggle="tab" data-target="#mic" class="ae fa label label-primary fa-microphone ">☆</label>`); $('.u-msg.dots').removeClass('dots'); $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="jfa-w" name="FirstName" value=" الأخبــــار "><iframe class="fr" style="width:100%;margin-top: 0px;" src="https://www.bbc.com/ws/av-embeds/media/bbc_arabic_radio/liveradio/ar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo ("#mic"),clearInterval(b)):console.log(c)},1)}); 
+
+vchat=true; $(".ustat").css("width",'12px').css('height','12px').css('border-radius','3em'); 
+
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+$(`<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:#F7E7E7;">
+<img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url;" class="fl fitimg hand u-pic ">
+<div class="uzr fl" style="padding:0px;width:80%">
+<div style="padding:0px;width:100%;" class="fl">
+<img class="fl u-ico" alt="" src="">
+<span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">‎ .. ' شآت ابو عريش  ، ،</span>
+</div>
+<br>
+<div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg break fl">
+<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg break fl"> مرحباً بك عزيزي الزآئر 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+</div>
+</div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);}) 
